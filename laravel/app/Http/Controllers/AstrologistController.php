bac85a43eefe0ea891ccdb208b413c27a709de98
Yehor <egor.gnedash@gmail.com>
2021-04-24 23:03:12 +0300

    ASTR-2: routes for astrologists

--- laravel/app/Http/Controllers/AstrologistController.php
+++ laravel/app/Http/Controllers/AstrologistController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Astrologist;
 use Illuminate\Http\Request;
 
 class AstrologistController extends Controller
 {
-    //
+    public function all()
+    {
+        $data = Astrologist::all()->map( function ($item) {
+            return [
+//                'photo' => $item->photo,
+                'name' => $item->name,
+                'services' => $item->services->pluck('name'),
+                'detailsUrl' => route('api.astrologists.show', $item->id)
+            ];
+        });
+
+        return response()->json($data);
+    }
+
+    public function show(Astrologist $astrologist)
+    {
+        $services = $astrologist->services->map( function ($service) {
+            return [
+                'name' => $service->name,
+                'price' => $service->pivot->price,
+                // route for buying
+                'buyUrl' => 'kupit uslugu'
+            ];
+        });
+        $data = [
+//                'photo' => $item->photo,
+            'name' => $astrologist->name,
+            'biography' => $astrologist->biography,
+            'services' => $services,
+        ];
+
+        return response()->json($data);
+    }
 }
