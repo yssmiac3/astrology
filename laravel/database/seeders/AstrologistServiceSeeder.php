6dfe3b016a1bccd83f324677c0429dd7fa82c1e2
Yehor <egor.gnedash@gmail.com>
2021-04-24 20:41:23 +0300

    comments deleted

--- laravel/database/seeders/AstrologistServiceSeeder.php
+++ laravel/database/seeders/AstrologistServiceSeeder.php
@@ -18,23 +18,11 @@ class AstrologistServiceSeeder extends Seeder
         $services = Service::all();
 
         Astrologist::all()->each(function ($astrologist) use ($services) {
-//            dd($services->random(mt_rand(1, 3))->pluck('id')
-//                ->map( function ($id) {
-//                    return [$id => ['price' => mt_rand(1, 100)]];
-//                })
-//            ->collapse());
             $astrologist->services()->attach(
-
                 $services->random(mt_rand(1, 3))->pluck('id')
                 ->mapWithKeys( function ($id) {
                     return [$id => ['price' => mt_rand(1, 100)]];
                 })
-
-//                    array_map(
-//                        fn($x) => [$x => ['price' => mt_rand(1, 100)]],
-//                        $services->random(mt_rand(1, 3))->pluck('id')->toArray()
-//                    )
-
             );
         });
     }
